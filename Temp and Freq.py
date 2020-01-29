644bf7d61665992842b97290b015107cb9c5e4db
Kashyap-Ravichandran97 <kashyapravichandran@gmail.com>
2020-01-28 19:29:07 -0500

    Axis Labels and made it look pretty

--- Temp and Freq.py
+++ Temp and Freq.py
@@ -1,7 +1,11 @@
 from gpiozero import CPUTemperature
 from time import sleep, strftime, time
+import matplotlib
 import matplotlib.pyplot as plt
 import subprocess
+import os
+
+#matplotlib.use('GTK')
 
 plt.ion()
 x = []
@@ -24,14 +28,16 @@ with open("/home/pi/cpu_temp.csv", "a") as log:
         plt.clf()
         plt.subplot(2,1,1)
         plt.plot(x,y)
+        #plt.xlabel("Time (s)")
+        plt.ylabel("Temperature")
         plt.subplot(2,1,2)
         plt.plot(x,z)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Frequency in GHz")
         log.write("{0},{1}\n".format(strftime("%Y-%m-%d: %H:%M:%S"),str(temp)))
         plt.pause(1)
         i=i+1
-        print(i)
+        #print(i)
         plt.draw()
-        if i > 30:
-                     x.pop(0);
-                     y.pop(0);
-                     z.pop(0);
+            
+
